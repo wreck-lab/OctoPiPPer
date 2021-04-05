83ee2c052bbfd821e658cb72bba43df9c929561d
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-04-05 18:36:31 +0200

    back to master because of f401 support not available in 0.9.1

--- src/modules/klipper/filesystem/home/pi/install-config.sh
+++ src/modules/klipper/filesystem/home/pi/install-config.sh
@@ -6,8 +6,8 @@ KLIPPER_PRINTHAT_VER="v1"
 set -e
 
 echo "--- Configuring printHAT ${KLIPPER_PRINTHAT_VER} ..."
-cp /home/pi/klipper_config/phat${KLIPPER_PRINTHAT_VER}_defconfig /home/pi/klipper/.config
-cp /home/pi/klipper_config/config/generic-wrecklab-printhat-${KLIPPER_PRINTHAT_VER}-cartesian.cfg /home/pi/printer.cfg
+cp /home/pi/klipper_wl/phat${KLIPPER_PRINTHAT_VER}_defconfig /home/pi/klipper/.config
+cp /home/pi/klipper_wl/config/generic-wrecklab-printhat-${KLIPPER_PRINTHAT_VER}-cartesian.cfg /home/pi/printer.cfg
 
 echo "--- Compiling Klipper firmware..."
 cd /home/pi/klipper
