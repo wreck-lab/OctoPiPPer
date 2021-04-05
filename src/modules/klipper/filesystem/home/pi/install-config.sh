62dc442bc60ffa8f2c7b739b8ac67309458f66ef
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-04-05 19:57:02 +0200

    fixed ln -s

--- src/modules/klipper/filesystem/home/pi/install-config.sh
+++ src/modules/klipper/filesystem/home/pi/install-config.sh
@@ -8,7 +8,7 @@ set -e
 echo "--- Configuring printHAT ${KLIPPER_PRINTHAT_VER} ..."
 cp /home/pi/klipper_config/phat${KLIPPER_PRINTHAT_VER}_defconfig /home/pi/klipper/.config
 cp /home/pi/klipper_config/config/generic-wrecklab-printhat-${KLIPPER_PRINTHAT_VER}-cartesian.cfg /home/pi/klipper_config/printer.cfg
-ln -s /home/pi/printer.cfg /home/pi/klipper_config/printer.cfg
+ln -s /home/pi/klipper_config/printer.cfg /home/pi/printer.cfg
 
 echo "--- Compiling Klipper firmware..."
 cd /home/pi/klipper
