5325fa4cf3575582a15e75d4d4ea58649d3f3c34
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-04-05 03:17:54 +0200

    drg

--- src/modules/klipper/filesystem/home/pi/install-config.sh
+++ src/modules/klipper/filesystem/home/pi/install-config.sh
@@ -5,21 +5,19 @@ KLIPPER_PRINTHAT_VER="v1"
 install_phat_v1()
 {
     echo "--- Configuring printHAT v1"
+	cp /home/pi/klipper_config/phatv1_defconfig /home/pi/klipper/.config
 	cd /home/pi/klipper
-	cp phatv1_defconfig .config
     make
-    cd ..
-    cp klipper/config/generic-wrecklab-printhat-v1-cartesian.cfg printer.cfg
+    cp /home/pi/klipper_config/config/generic-wrecklab-printhat-v1-cartesian.cfg printer.cfg
 }
 
 install_phat_v2()
 {
     echo "--- Configuring printHAT v2"
+	cp /home/pi/klipper_config/phatv2_defconfig /home/pi/klipper/.config
 	cd /home/pi/klipper
-	cp phatv2_defconfig .config
     make
-    cd ..
-    cp klipper/config/generic-wrecklab-printhat-v2-cartesian.cfg printer.cfg
+    cp /home/pi/klipper_config/config/generic-wrecklab-printhat-v2-cartesian.cfg printer.cfg
 }
 
 # Force script to exit if an error occurs
