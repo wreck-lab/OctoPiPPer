f4907dfc34b896f9bfe631173fdd11becac4d444
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-04-05 12:31:43 +0200

    generalized config script

--- src/modules/klipper/filesystem/home/pi/install-config.sh
+++ src/modules/klipper/filesystem/home/pi/install-config.sh
@@ -2,32 +2,13 @@
 
 KLIPPER_PRINTHAT_VER="v1"
 
-install_phat_v1()
-{
-    echo "--- Configuring printHAT v1"
-	cp /home/pi/klipper_config/phatv1_defconfig /home/pi/klipper/.config
-	cd /home/pi/klipper
-    make
-    cp /home/pi/klipper_config/config/generic-wrecklab-printhat-v1-cartesian.cfg printer.cfg
-}
-
-install_phat_v2()
-{
-    echo "--- Configuring printHAT v2"
-	cp /home/pi/klipper_config/phatv2_defconfig /home/pi/klipper/.config
-	cd /home/pi/klipper
-    make
-    cp /home/pi/klipper_config/config/generic-wrecklab-printhat-v2-cartesian.cfg printer.cfg
-}
-
 # Force script to exit if an error occurs
 set -e
 
-# files in home
-if [ "$KLIPPER_PRINTHAT_VER" == "v1" ]
-then
-	install_phat_v1
-elif [ "$KLIPPER_PRINTHAT_VER" == "v2" ] 
-then
-	install_phat_v2
-fi
+echo "--- Configuring printHAT ${KLIPPER_PRINTHAT_VER} ..."
+cp /home/pi/klipper_config/phat${KLIPPER_PRINTHAT_VER}_defconfig /home/pi/klipper/.config
+cp /home/pi/klipper_config/config/generic-wrecklab-printhat-${KLIPPER_PRINTHAT_VER}-cartesian.cfg /home/pi/printer.cfg
+
+echo "--- Compiling Klipper firmware..."
+cd /home/pi/klipper
+make
