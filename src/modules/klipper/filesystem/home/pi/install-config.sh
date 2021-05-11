dbdd1b0172b266376766df434dfa8e5f9d4c179a
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-05-11 14:01:01 +0200

    Update install-config.sh

--- src/modules/klipper/filesystem/home/pi/install-config.sh
+++ src/modules/klipper/filesystem/home/pi/install-config.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-KLIPPER_PRINTHAT_VER="v1"
-
 # Force script to exit if an error occurs
 set -e
 
-echo "--- Configuring printHAT ${KLIPPER_PRINTHAT_VER} ..."
-cp /home/pi/klipper_config/phat${KLIPPER_PRINTHAT_VER}_defconfig /home/pi/klipper/.config
-cp /home/pi/klipper_config/config/generic-wrecklab-printhat-${KLIPPER_PRINTHAT_VER}-cartesian.cfg /home/pi/klipper_config/printer.cfg
+echo "--- Configuring printHAT $(phat_ver) ..."
+cp /home/pi/klipper_config/phat$(phat_ver)_defconfig /home/pi/klipper/.config
+cp /home/pi/klipper_config/config/generic-wrecklab-printhat-$(phat_ver)-cartesian.cfg /home/pi/klipper_config/printer.cfg
 ln -s /home/pi/klipper_config/printer.cfg /home/pi/printer.cfg
 
 echo "--- Compiling Klipper firmware..."
