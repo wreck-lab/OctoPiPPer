5c66a5b53f343452b67941fcae1a611da154b660
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-05-11 17:54:39 +0200

    Update install-config.sh

--- src/modules/klipper/filesystem/home/pi/install-config.sh
+++ src/modules/klipper/filesystem/home/pi/install-config.sh
@@ -3,6 +3,8 @@
 # Force script to exit if an error occurs
 set -e
 
+PHAT_VERSION=v2
+
 echo "--- Configuring printHAT $(PHAT_VERSION)..."
 cp /home/pi/klipper_config/phat${PHAT_VERSION}_defconfig /home/pi/klipper/.config
 cp /home/pi/klipper_config/config/generic-wrecklab-printhat-${PHAT_VERSION}-cartesian.cfg /home/pi/klipper_config/printer.cfg
