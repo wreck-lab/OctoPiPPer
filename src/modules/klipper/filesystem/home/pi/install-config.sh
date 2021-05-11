cb891e41a59b3724f82f0e8783aeae65638f04ab
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-05-11 18:37:17 +0200

    Update install-config.sh

--- src/modules/klipper/filesystem/home/pi/install-config.sh
+++ src/modules/klipper/filesystem/home/pi/install-config.sh
@@ -3,7 +3,7 @@
 # Force script to exit if an error occurs
 set -e
 
-PHAT_VERSION = $1
+PHAT_VERSION=$1
 
 echo "--- Configuring printHAT ${PHAT_VERSION}..."
 cp /home/pi/klipper_config/phat${PHAT_VERSION}_defconfig /home/pi/klipper/.config
