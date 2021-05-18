e66ca22c33842ddd8b0552d133f15a739cd51250
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-05-18 18:56:45 +0200

    Update update_v2.sh

--- src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
+++ src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
@@ -1,30 +1,45 @@
 #!/bin/bash
-# optional first argument: 'wrecklab' or 'klipper'
-# $KLIPPER_UPDATE = $1
 
 cd $HOME/klipper
 
-# get the flavor currently installed
-#if [[ $(git config --get remote.origin.url) == *"KevinOConnor"* ]]
-#then
-#  $KLIPPER_INSTALL = 'klipper'
-#else
-#  $KLIPPER_INSTALL = 'wrecklab'
-#fi
+TAG=$(git describe --tags --abbrev=0)
+echo $TAG
 
-# update local
-git pull #https://github.com/KevinOConnor/klipper.git
-#git pull https://github.com/wreck-lab/klipper.git
+git fetch --tags
+TAG_NEW=$(git describe --tags --abbrev=0)
+echo $TAG_NEW
+
+if [ "$TAG" != "$TAG_NEW" ]
+then
+  git checkout "$TAG_NEW"
+fi
 
 # assume klipper config options have not changed
-make
+/usr/bin/make
 
-## flash board
-# (remember to put jumper before and remove it after)
+# confirm jumper
+read -p "Install the BOOT jumper and continue (Y/n)" jummper
+jumper=${jumper:-y}
+
+if [ $jumper != "y" ]
+then
+  exit
+fi
+
+sudo service klipper stop
+
+# reset the board
 echo "18" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio18/direction
 echo "1" > /sys/class/gpio/gpio18/value
 
+# flash the board
 /usr/bin/make serialflash FLASH_DEVICE=/dev/ttyAMA0
 
+# clear the reset
 echo "0" > /sys/class/gpio/gpio18/value
+
+# confirm no jumper
+read -p "Remove the BOOT jumper and press ENTER" jumper
+
+sudo service klipper start
