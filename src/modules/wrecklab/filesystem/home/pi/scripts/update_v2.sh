e3fa0f14b15291d43448beeae08c382018e10238
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-05-20 17:55:00 +0200

    Update update_v2.sh

--- src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
+++ src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
@@ -1,45 +1,76 @@
 #!/bin/bash
 
+# check conditions for execution
+if [ -f "/boot/firstrun" ]; then
+ echo "Found firstrun file. Updating..."
+else
+ if [ -f "/boot/firstrun.txt" ]; then
+  echo "Found firstrun.txt file. Updating..."
+ else
+  if [ ! -z "$1" -a "$1" == "f" ]; then
+   echo "Force update. Updating..."
+  else
+   echo "No firstrun file or force update flag."
+   exit
+  fi
+ fi
+fi
+
 cd $HOME/klipper
+REMOTE="https://github.com/wreck-lab/klipper.git"
 
 TAG=$(git describe --tags --abbrev=0)
 echo $TAG
 
-git fetch --tags
-TAG_NEW=$(git describe --tags --abbrev=0)
+# get remote tags
+TAG_NEW=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $REMOTE '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3)
 echo $TAG_NEW
 
-if [ "$TAG" != "$TAG_NEW" ]
+if [ -z "$TAG" -o -z "$TAG_NEW" -o  "$TAG" == "$TAG_NEW" ]
 then
-  git checkout "$TAG_NEW"
+  echo  "Nothing to update. Exit."
+  exit
 fi
+
+git pull
+git checkout "$TAG_NEW" -b "$TAG_NEW"
 
 # assume klipper config options have not changed
 /usr/bin/make
 
+## flash board
 # confirm jumper
 read -p "Install the BOOT jumper and continue (Y/n)" jummper
 jumper=${jumper:-y}
 
 if [ $jumper != "y" ]
 then
-  exit
+ exit
 fi
 
+# stop klipper
 sudo service klipper stop
 
-# reset the board
-echo "18" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio18/direction
-echo "1" > /sys/class/gpio/gpio18/value
+# reset board
+echo 4 > /sys/class/gpio/export
+sleep 0.1
+echo out > /sys/class/gpio/gpio4/direction
+echo 1 > /sys/class/gpio/gpio4/value
+sleep 0.5
+echo 0 > /sys/class/gpio/gpio4/value
+sleep 0.5
+echo 1 > /sys/class/gpio/gpio4/value
 
-# flash the board
+# flash
 /usr/bin/make serialflash FLASH_DEVICE=/dev/ttyAMA0
 
-# clear the reset
-echo "0" > /sys/class/gpio/gpio18/value
+echo 4 > /sys/class/gpio/unexport
 
-# confirm no jumper
 read -p "Remove the BOOT jumper and press ENTER" jumper
 
+# start klipper
 sudo service klipper start
+
+echo "Done."
+
+
