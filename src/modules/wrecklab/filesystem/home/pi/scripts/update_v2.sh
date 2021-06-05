076fdc3e69c3fee3d9d29b17a8a60a473123aa13
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-06-05 11:59:35 +0200

    updated update

--- src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
+++ src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SELF=$(basename "$0")
-FORCE=false
+AUTO=false
 BRANCH="master"
 LOCAL=$HOME/klipper
 REPO_REM="wreck-lab/klipper"
@@ -13,7 +13,7 @@ usage() {
   cat << EOF >&2
 Usage: $SELF [-v] [-d <dir>] [-f <file>]
  -b <branch>: branch to pull (default to master)
- -f         : force update and flash
+ -a         : auto, no user prompt
 EOF
   exit 1
 }
@@ -24,7 +24,23 @@ log_date() {
   done
 }
 
-vercomp() {
+set_time_net() {
+  # Enables NTP to update date and time from internet
+  timedatectl set-ntp True | log_date >> $LOG 2>&1
+}
+
+check_network() {
+  echo -n "Checking network connectivity... "
+  if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+    echo "OK"
+    NET=1
+  else
+    # no need for echo, will get the error message
+    NET=0
+  fi
+}
+
+check_ver() {
     if [[ $1 == $2 ]]; then
         return 0
     fi
@@ -53,27 +69,27 @@ vercomp() {
     return 0
 }
 
-selfUpdate() {
-  echo "Performing self-update..."
+update_script() {
+  echo "Starting script update... "
 
   # Download new version
-  echo -n "Downloading latest version..."
+  echo -n "Downloading latest version... "
   if ! wget -O --quiet --output-document="$SELF.tmp" $SELF_REM ; then
     echo "Failed: Error while trying to wget new version!"
     echo "File requested: https://raw.githubusercontent.com/"$REPO_REM"/master/"$SELF
     exit 1
   fi
-  echo "Done."
+  echo "OK"
 
-  # Check for new versions
+  # use size to detect new file
   SIZE_LOC=$(stat --printf="%s" "$SELF")
   SIZE_REM=$(stat --printf="%s" "$SELF.tmp")
 
   if [ "$SIZE_LOC" -eq "$SIZE_REM" ]; then
-    echo "Local version already latest."
+    echo "Local version already up to date."
     return
   else
-    echo "Updating to latest version..."
+    echo "Updating script to latest version... "
   fi
 
   # Copy over modes from old version
@@ -96,103 +112,111 @@ else
   echo "Failed!"
 fi' > selfup.sh
 
-  echo -n "Inserting update process..."
+  echo -n "Inserting update process... "
   exec /bin/bash selfup.sh
 }
 
+update_system() {
+  # this update must run in the repo folder
+
+  # get local and remote info
+  TAG_LOC=$(git describe --tags --abbrev=0)
+  TAG_REM=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags "https://github.com/"$REPO_REM '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3)
+
+  if [ -z "$TAG_REM" ]; then
+    echo "Remote returned an empty message. Network might have gone down. Falling back to remote version 0.0.0"
+    TAG_REM=0.0.0
+  fi
+
+  # purge non digit (e.g. initial v)
+  TAG_REM=$( echo $TAG_REM | sed 's/v//')
+  TAG_LOC=$( echo $TAG_LOC | sed 's/v//')
+
+  if [ "$BRANCH" != "master" ]; then
+    echo "Update to the remote branch: $BRANCH, requested... "
+    echo "Pulling remote branch: $BRANCH ... "
+    git pull origin "$BRANCH"
+    git checkout "$BRANCH"
+
+  else
+    echo "Update on the master branch requested... "
+    check_ver "$TAG_LOC" "$TAG_REM"
+    if [ "$?" = "2" ]; then
+      echo "Remote version: $TAG_REM is newer than local: $TAG_LOC. Updating... "
+      git checkout "$TAG_REM" -b "$TAG_REM"
+    else
+      echo "Remote version: $TAG_REM, Local version: $TAG_LOC. Already up to date."
+    fi
+
+  fi
+}
+
+update_klipper() {
+  # this update must run in the repo folder
+
+  # assume klipper config options have not changed
+  /usr/bin/make clean | log_date >> $LOG 2>&1
+  /usr/bin/make | log_date >> $LOG 2>&1
+
+  # confirm jumper
+  while [ "$AUTO" = "false" ]; do
+      read -p "Install the BOOT jumper and continue (Y/n): " yn
+      case $yn in
+          [Yy]* ) break;;
+          [Nn]* ) exit;;
+          * ) break;;
+      esac
+  done
+
+  # stop klipper
+  echo -n "Stopping Klipper service... "
+  sudo service klipper stop
+  echo "OK"
+
+  # reset board
+  $HOME/scripts/reset_pin_cycle.sh
+
+  # flash
+  /usr/bin/make serialflash FLASH_DEVICE=/dev/ttyAMA0 | log_date >> $LOG 2>&1
+
+  if [ "$AUTO" = "false" ]; then
+    read -p "Remove the BOOT jumper and press any key to continue: " jumper
+  fi
+
+  # start klipper
+  echo -n "Staring Klipper service... "
+  sudo service klipper start
+  echo "OK"
+
+}
+
+
+## SCRIPT STARTS HERE
+echo "Updating... "
+touch $LOG
+
+# check connectivity first
+check_network
 
 # parse arguments
-while getopts fub: o; do
+while getopts ab: o; do
   case $o in
-    (f) FORCE=true;;
     (b) BRANCH=$OPTARG;;
-    (u) selfUpdate;;
+    (a) AUTO=true;;
     (*) usage
   esac
 done
 
-# self update
-selfUpdate
+if [ $NET -eq "1" ]; then
+  # try script update
+  update_script
+  # try system update
+  cd $LOCAL
+  update_system
+fi
 
-# create log file, if not there
-touch $LOG
-
-# get local and remote info
 cd $LOCAL
-TAG_LOC=$(git describe --tags --abbrev=0)
-TAG_REM=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags "https://github.com/"$REPO_REM '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3)
+update_klipper
 
-# check connection
-if [ -z "$TAG_REM" ]; then
-  echo "Remote returned an empty message. Network might be down. Falling back to remote version 0.0.0"
-  TAG_REM=0.0.0
-fi
-
-# purge non digit (e.g. initial v)
-TAG_REM=$( echo $TAG_REM | sed 's/v//')
-TAG_LOC=$( echo $TAG_LOC | sed 's/v//')
-
-
-# check conditions for execution
-if [[ -n "$(find /boot -name 'firstrun*' | head -1)"  || "$FORCE" = "true" ]]; then
-  if [ -n "$(find /boot -name 'firstrun*' | head -1)" ]; then
-    /usr/bin/rm -f /boot/firstrun* | log_date >> $LOG 2>&1
-    # Enables NTP to update date and time from internet
-    timedatectl set-ntp True | log_date >> $LOG 2>&1
-  fi
-
-  echo " An update on the master branch has been requested."
-  vercomp "$TAG_LOC" "$TAG_REM"
-  if [ "$?" = "2" ]; then
-    echo " Remote version: $TAG_REM is newer than local: $TAG_LOC. Updating..."
-    git checkout "$TAG_REM" -b "$TAG_REM"
-  else
-    echo " Remote version: $TAG_REM, Local version: $TAG_LOC. Already up to date."
-    exit
-  fi
-
-elif [ "$BRANCH" != "master" ]; then
-  echo " An update to the remote branch: $BRANCH, has been requested."
-  echo " Pulling remote branch: $BRANCH ..."
-  git pull origin "$BRANCH"
-  git checkout "$BRANCH"
-  FORCE=true
-
-else
-  echo "Not a firstrun. Use \"update_v2 sh -f\" to force an update."
-  exit
-fi
-
-# assume klipper config options have not changed
-/usr/bin/make clean | log_date >> $LOG 2>&1
-/usr/bin/make | log_date >> $LOG 2>&1
-
-# confirm jumper
-while [ "$FORCE" = "true" ]; do
-    read -p "Install the BOOT jumper and continue (Y/n): " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) break;;
-    esac
-done
-
-# stop klipper
-echo "Stopping Klipper service..."
-sudo service klipper stop
-
-# reset board
-$HOME/scripts/reset_pin_cycle.sh
-
-# flash
-/usr/bin/make serialflash FLASH_DEVICE=/dev/ttyAMA0 | log_date >> $LOG 2>&1
-
-if [ "$FORCE" = "true" ]; then
-  read -p "Remove the BOOT jumper and press any key to continue:" jumper
-fi
-
-# start klipper
-echo "Staring Klipper service..."
-sudo service klipper start
-
-echo "All done. Bye"
+echo "Done"
+exit 0
