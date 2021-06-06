819a8e3e2c39511f9ea014c7b70d016094d5af92
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-06-07 00:13:30 +0200

    latest

--- src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
+++ src/modules/wrecklab/filesystem/home/pi/scripts/update_v2.sh
@@ -100,7 +100,7 @@ update_script() {
 
   # Copy over modes from old version
   OCTAL_MODE=$(stat -c '%a' "$SCRIPTS/$SELF")
-  if ! chmod $OCTAL_MODE "$0.tmp" ; then
+  if ! chmod $OCTAL_MODE "$SCRIPTS/$SELF.tmp" ; then
     echo "Failed: Error while trying to set mode on $0.tmp."
     exit 1
   fi
