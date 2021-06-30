4de770dc3d00dd9a63eda418180cad048fa79066
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-06-30 17:11:23 +0200

    test sed replacement

--- src/test_var.sh
+++ src/test_var.sh
@@ -1,2 +1,9 @@
-echo "$GUI"
-echo "$MODULES"
+#!/bin/bash
+
+if [ "$GUI" = "octoprint" ]; then
+  sed 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=3000/' ./config
+  sed 's/^.*\bMODULES\b.*$/export MODULES="base(raspicam, network, klipper, disable-services(octopi, auto-hotspot, wrecklab), password-for-sudo)"/' ./config
+else
+  sed 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=2000/' ./config
+  sed 's/^.*\bMODULES\b.*$/export MODULES="base(network, raspicam, klipper, moonraker, mainsail, mjpgstreamer, disable-services(auto-hotspot, wrecklab), password-for-sudo)"/' ./config
+fi
