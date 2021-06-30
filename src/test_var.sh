fec119a3e7755bcf2396029a76af472266cd2743
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-06-30 17:27:09 +0200

    try run with sed

--- src/test_var.sh
+++ src/test_var.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 if [ "$GUI" = "octoprint" ]; then
-  sed -i 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/stefaninos_octoprint/' ./config
-  sed -i 's/^.*\bMODULES\b.*$/stefaninos_modules/' ./config
+  sed -i 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=3000/' ./config
+  sed -i 's/^.*\bMODULES\b.*$/export MODULES="base(raspicam, network, klipper, disable-services(octopi, auto-hotspot, wrecklab), password-for-sudo)"/' ./config
 else
   sed -i 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=2000/' ./config
   sed -i 's/^.*\bMODULES\b.*$/export MODULES="base(network, raspicam, klipper, moonraker, mainsail, mjpgstreamer, disable-services(auto-hotspot, wrecklab), password-for-sudo)"/' ./config
 fi
-
-#stefaninos_octoprint
-# export BASE_IMAGE_ENLARGEROOT=3000
-# export MODULES="base(raspicam, network, klipper, disable-services(octopi, auto-hotspot, wrecklab), password-for-sudo)"
