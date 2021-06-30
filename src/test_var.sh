2272e13cd69253d9d87a8384bc38cd15c770a998
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-06-30 17:23:33 +0200

    sed -i

--- src/test_var.sh
+++ src/test_var.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 if [ "$GUI" = "octoprint" ]; then
-  sed 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=3000/' ./config
-  sed 's/^.*\bMODULES\b.*$/export MODULES="base(raspicam, network, klipper, disable-services(octopi, auto-hotspot, wrecklab), password-for-sudo)"/' ./config
+  sed -i 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/stefaninos_octoprint/' ./config
+  sed -i 's/^.*\bMODULES\b.*$/stefaninos_modules/' ./config
 else
-  sed 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=2000/' ./config
-  sed 's/^.*\bMODULES\b.*$/export MODULES="base(network, raspicam, klipper, moonraker, mainsail, mjpgstreamer, disable-services(auto-hotspot, wrecklab), password-for-sudo)"/' ./config
+  sed -i 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=2000/' ./config
+  sed -i 's/^.*\bMODULES\b.*$/export MODULES="base(network, raspicam, klipper, moonraker, mainsail, mjpgstreamer, disable-services(auto-hotspot, wrecklab), password-for-sudo)"/' ./config
 fi
+
+stefaninos_octoprint
+# export BASE_IMAGE_ENLARGEROOT=3000
+# export MODULES="base(raspicam, network, klipper, disable-services(octopi, auto-hotspot, wrecklab), password-for-sudo)"
