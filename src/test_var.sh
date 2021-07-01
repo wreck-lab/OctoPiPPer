bc8ce8cef8215818c2ff2d04d3b0635a4a3e47e2
Stefano Cottafavi <stefano.cottafavi@gmail.com>
2021-07-01 20:35:13 +0200

    fix sed

--- src/test_var.sh
+++ src/test_var.sh
@@ -7,7 +7,7 @@ else
   sed -i 's/^.*\bBASE_IMAGE_ENLARGEROOT\b.*$/export BASE_IMAGE_ENLARGEROOT=2000/' ./config
   sed -i 's/^.*\bMODULES\b.*$/export MODULES="base(network, raspicam, klipper, moonraker, mainsail, mjpgstreamer, disable-services(auto-hotspot, wrecklab), password-for-sudo)"/' ./config
   for filename in modules/klipper/filesystem/home/pi/klipper_config/config/*.cfg; do
-    sed -i -e '$a ' "$filename"
+    sed -i -e '$a\' "$filename"
     sed -i -e '$a[include mainsail.cfg]' "$filename"
   done
 fi
